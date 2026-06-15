c072348ad12e1807f1cffa0ccbb377687d85e706
Rock411 <yangln@126.com>
2026-06-15 17:55:23 +0200

    Update 1_geometry_and_mask.py

--- StitchGrids/1_geometry_and_mask.py
+++ StitchGrids/1_geometry_and_mask.py
@@ -2,10 +2,10 @@ import numpy as np
 import os
 
 # ===== Set File Path =====
-data_dir = 'C:/Users/yangln/Desktop/Postdoc/CNR_Italy/Maps/BouguerAnomaly'
+data_dir = 'C:/.../BouguerAnomaly'
 land_file = os.path.join(data_dir, 'BouguerLandGrid.asc')
 sea_file = os.path.join(data_dir, 'BouguerSeaGrid.asc')
-output_dir = 'C:/Users/yangln/Desktop/Postdoc/CNR_Italy/Maps/BouguerAnomaly/StitchGrids'
+output_dir = 'C:/.../BouguerAnomaly/StitchGrids'
 # =========================
 
 def read_asc_header(filename):
